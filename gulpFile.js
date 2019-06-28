8a3cf775936aae1ece1fff4497f4a391788b88f9
Vigneshwaran J <vigneshwaran.j@msil.ad>
2019-06-28 12:00:38 +0530

    Updated commit that supports assets folder update with sub folders transfer

--- gulpFile.js
+++ gulpFile.js
@@ -7,6 +7,7 @@ var htmlMin = require("gulp-htmlmin");
 var browserSync = require("browser-sync").create();
 var concat = require("gulp-concat");
 var replace = require("gulp-replace");
+var del = require("del");
 //bundle
 var browserify = require("browserify");
 var babelify = require("babelify");
@@ -19,13 +20,6 @@ var dest = "dist/";
 var public = "public/";
 var jsFiles = [src+"index.js", src+"App.js",components+"OverrideEvents.js", components+"WebSocket.js"];
 
-//file related variables
-var fileNames = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let randValue="";
-for(var i=0;i<7;i++)
-{
-  randValue += fileNames[Math.floor(Math.random() * fileNames.length)];
-}
 
 
 //tasks
@@ -34,32 +28,38 @@ gulp.task("js" , function()
  return gulp.src(jsFiles)
   .pipe(babel())
   .pipe(uglify())
-  .pipe(concat(randValue+".min.js",{newLine:";"}))
+  .pipe(concat("minified.min.js",{newLine:";"}))
   .pipe(gulp.dest(dest+"static/js/")) 
 });
 
+gulp.task("clean", function(){
+  return del([dest+"static/assets"+"index.html"])
+})
 
 gulp.task("style", function() {
+  del([dest+"static/css"])
   return gulp.src(src+"styles/*.css")
   .pipe(minifyCss({keepSpecialComments : 1}))
-  .pipe(concat(randValue+".min.css",{newLine:";"}))
+  .pipe(concat("bundleCSS.min.css",{newLine:";"}))
   .pipe(gulp.dest(dest+"static/css/"))
 })
 
 gulp.task("pages", function(){
   var scriptSrc = `<script src='static/js/bundle.js'></script></body>`;
-  var cssSrc = `<link rel='stylesheet' href='static/css/${randValue}.min.css' /><title>`;
+  var cssSrc = "<link rel='stylesheet' href='static/css/bundleCSS.min.css' /><title>";
   return gulp.src(public+"*.html")
   .pipe(htmlMin({collapseWhitespace:true}))
   .pipe(replace("%PUBLIC_URL%/", ""))
+  .pipe(replace("favicon.ico", "static/assets/favicon.ico"))
+  .pipe(replace("manifest.json", "static/assets/manifest.json"))
   .pipe(replace("<title>", cssSrc))
   .pipe(replace("</body>", scriptSrc))
   .pipe(gulp.dest(dest));
 });
 
 gulp.task("assets", function(){
-  return gulp.src([public+"*.json", public+"*.ico"])
-  .pipe(gulp.dest(dest));
+  return gulp.src([public+"*", public+"**/*"])
+  .pipe(gulp.dest(dest+"static/assets"));
 });
 
 gulp.task("serve", function()
@@ -88,7 +88,7 @@ gulp.task("bundle", function(){
   .pipe(gulp.dest(dest+"static/js"))
 });
 
-gulp.task("default", gulp.series(gulp.parallel("bundle", "style", "pages", "serve")), function(){
+gulp.task("default", gulp.series(gulp.parallel("bundle", "style", "pages", "assets", "clean", "serve")), function(){
   return browserSync.reload({
     stream: true
   });
